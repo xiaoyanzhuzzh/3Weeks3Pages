bc4a66836ec10d8bfbc3b0adc09fbd426681158e
zhzhang <zhzhang@thoughtworks.com>
2016-09-20 14:50:54 +0800

    Using gulp.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('server', function(){
+    return gulp.src('./node_modules/font-awesome/**/*')
+      .pipe(gulp.dest('./dist/library/font-awesome/'));
+})
+
+gulp.task('clean', function(){
+    return del(['./dist']);
+})
+
+gulp.task('default', ['clean']);
